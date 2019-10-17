e77ff00d5a93c73cb28fb5212d84bb7b55ecf853
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2019-10-16 21:28:01 -0500

    Ignore files with escape set.

--- random-test.sh
+++ random-test.sh
@@ -6,5 +6,10 @@ tree-sitter generate
 
 for RANDOM_TARGET in $(find ./tests -type f | shuf | head -n100); do
   cat "${RANDOM_TARGET}" > target.Dockerfile
+  
+  if cat target.Dockerfile | grep -q 'escape=`'; then
+    continue
+  fi
+
   tree-sitter parse "${RANDOM_TARGET}"
 done
