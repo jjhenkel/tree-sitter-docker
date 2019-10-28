a7562be5f3a06f547516c18a574cfbd637e90c88
Jordan Henkel <jjhenkel@cs.wisc.edu>
2019-10-28 15:43:34 -0500

    About 2000 erroring files left in corpus.

--- random-test.sh
+++ random-test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+export PATH=$PATH:`pwd`/node_modules/.bin/
+
 tree-sitter generate
 
-for RANDOM_TARGET in $(find ./sources -type f | shuf); do
-  echo "${RANDOM_TARGET}"
+for RANDOM_TARGET in $(cat ./errors.txt); do
   tree-sitter parse "${RANDOM_TARGET}" > ./asts/$(basename "${RANDOM_TARGET}").ast
+  grep -qF 'ERROR' ./asts/$(basename "${RANDOM_TARGET}").ast && echo "${RANDOM_TARGET}"
 done
