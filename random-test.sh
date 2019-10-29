6550af30a58182e6c6f384d7203cc88c968b12b8
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2019-10-29 17:55:47 -0500

    Updated errors again. And some utils.

--- random-test.sh
+++ random-test.sh
@@ -5,6 +5,7 @@ export PATH=$PATH:`pwd`/node_modules/.bin/
 tree-sitter generate
 
 for RANDOM_TARGET in $(cat ./errors.txt); do
+  echo "${RANDOM_TARGET}" >&2
   tree-sitter parse "${RANDOM_TARGET}" > ./asts/$(basename "${RANDOM_TARGET}").ast
   grep -qF 'ERROR' ./asts/$(basename "${RANDOM_TARGET}").ast && echo "${RANDOM_TARGET}"
 done
